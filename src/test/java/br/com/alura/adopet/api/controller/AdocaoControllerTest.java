ce9f665e6374a72c5558a6ba4f2b2975f4f10fdb
LucasMartines01 <lucas.martines@izio.com.br>
2024-05-20 18:10:31 -0300

    fix: remove unecessary imports

--- src/test/java/br/com/alura/adopet/api/controller/AdocaoControllerTest.java
+++ src/test/java/br/com/alura/adopet/api/controller/AdocaoControllerTest.java
@@ -4,19 +4,16 @@ import br.com.alura.adopet.api.service.AdocaoService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 class AdocaoControllerTest {
