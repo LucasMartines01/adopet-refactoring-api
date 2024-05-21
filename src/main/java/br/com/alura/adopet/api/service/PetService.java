d770f14db697f75dd243fe6a578bdb0b2f326c1f
LucasMartines01 <lucas.martines@izio.com.br>
2024-05-20 23:21:33 -0300

    feat: fix dependency injection

--- src/main/java/br/com/alura/adopet/api/service/PetService.java
+++ src/main/java/br/com/alura/adopet/api/service/PetService.java
@@ -2,15 +2,18 @@ package br.com.alura.adopet.api.service;
 
 import br.com.alura.adopet.api.dto.PetResponseDto;
 import br.com.alura.adopet.api.repository.PetRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class PetService {
-    @Autowired
-    PetRepository petRepository;
+
+    final PetRepository petRepository;
+
+    public PetService(PetRepository petRepository) {
+        this.petRepository = petRepository;
+    }
 
     public List<PetResponseDto> listarTodosDisponiveis() {
         return petRepository.findAllByAdotadoFalse().stream().map(PetResponseDto::new).toList();
